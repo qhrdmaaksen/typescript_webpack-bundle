7c789964227b158b4417dec1765f500441e14369
vitamin777 <rlaalsdn8@gmail.com>
2023-03-09 23:57:56 +0900

    feat: webpack-dev-server & source-map 설정 추가
    -1- 실행 명령어 serve 로 webpack-dev-server 추가
    
    -2- source map 사용하기 위해 tsconfig.js 에서 sourceMap true 설정
    
    -3- 빌드 시 mode 설정 공백으로 인해 webpack.config.js 에 mode development 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
 const path = require("path");
 module.exports = {
+  // Webpack 의 모드를 설정
+  mode: "development",
   // 엔트리 포인트는 Webpack 에게 번들링을 시작할 애플리케이션의 시작점을 지정해 줌
   entry: "./src/index.ts",
+  // webpack.config.js 파일 내의 Webpack 에게 해당 소스 맵을 추출해 최종 번들에 포함하라고 지시
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./"),
+    },
+  },
   // 배열이 될 rules 프로퍼티가 포함되며, 여러 개의 규칙을 추가
   module: {
     rules: [
@@ -19,5 +28,6 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist",
   },
 };
